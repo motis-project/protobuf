6b0db398b7762a6562b09352606bb667437c1d87
Felix Gündling <felix.guendling@gmail.com>
2023-11-21 16:17:41 +0100

    system header, mac os cross compiling

--- cmake/protoc.cmake
+++ cmake/protoc.cmake
@@ -10,5 +10,17 @@ target_link_libraries(protoc
 )
 add_executable(protobuf::protoc ALIAS protoc)
 
+if (CMAKE_OSX_ARCHITECTURES)
+    set_target_properties(protoc PROPERTIES OSX_ARCHITECTURES "x86_64;arm64")
+endif()
+if(UNIX AND NOT APPLE)
+    target_link_libraries(protoc atomic)
+endif()
+target_link_libraries(protoc ${CMAKE_THREAD_LIBS_INIT})
+target_include_directories(protoc PRIVATE src ${CMAKE_CURRENT_BINARY_DIR})
+if (CMAKE_USE_PTHREADS_INIT)
+    target_compile_definitions(protoc PUBLIC HAVE_PTHREAD)
+endif (CMAKE_USE_PTHREADS_INIT)
+
 set_target_properties(protoc PROPERTIES
     VERSION ${protobuf_VERSION})
